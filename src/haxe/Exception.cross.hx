38a01e2b85e42f58fc0b0a4b95fb4395fbcf96a6
Ethantobot <ethantobot7@gmail.com>
2026-08-08 16:25:17 +0200

    Update Exception.cross.hx

--- src/haxe/Exception.cross.hx
+++ src/haxe/Exception.cross.hx
@@ -11,6 +11,9 @@ package haxe;
 @:headerInclude("haxe_CallStack.h", true)
 @:headerInclude("haxe_NativeStackTrace.h", true)
 @:headerCode("
+namespace haxe {
+    enum class StackItem;
+}
 #ifndef HCXX_STACK_METHOD
 #define HCXX_STACK_METHOD(...)
 #endif
