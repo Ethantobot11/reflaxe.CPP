b86ba36f5465f6db8ffad3eb4b41e15907b41f55
Ethantobot <ethantobot7@gmail.com>
2026-08-09 00:40:07 +0200

    Update Int32.hx

--- src/cxx/num/Int32.hx
+++ src/cxx/num/Int32.hx
@@ -11,4 +11,10 @@ package cxx.num;
 @:coreType
 @:notNull
 @:runtimeValue
-extern abstract Int32 to Int from Int {}
+extern abstract Int32 to Int from Int {
+    @:op(-A) private inline function negate():Int32 return -this;
+    @:op(A + B) private static inline function add(a:Int32, b:Int32):Int32 return a + b;
+    @:op(A - B) private static inline function sub(a:Int32, b:Int32):Int32 return a - b;
+    @:op(A * B) private static inline function mul(a:Int32, b:Int32):Int32 return a * b;
+    @:op(A / B) private static inline function div(a:Int32, b:Int32):Int32 return Std.int(a / b);
+}
