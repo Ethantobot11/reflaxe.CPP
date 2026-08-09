df9dfd66bbaab358253f58c125d9172ac7ccf2ee
Ethantobot <ethantobot7@gmail.com>
2026-08-09 02:06:18 +0200

    Update NativeInput.hx

--- src/cxx/io/NativeInput.hx
+++ src/cxx/io/NativeInput.hx
@@ -7,16 +7,6 @@ package cxx.io;
 @:headerInclude("algorithm", true)
 @:headerInclude("deque", true)
 @:headerInclude("memory", true)
-@:headerCode("
-#include <iostream>
-#include <algorithm>
-#include <deque>
-#include <memory>
-
-namespace haxe {
-    class NativeInt64Struct;
-}
-")
 class NativeInput extends haxe.io.Input {
 	var stream: Null<cxx.Ptr<cxx.std.ios.IStream>>;
 
